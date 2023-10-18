659aef6c7dc36222f19c4723a58625d0783462b4
DIVX <divx@XO-001.local>
2023-10-18 14:36:50 +0900

    controllerのcreateを作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,4 +6,16 @@ class BoardsController < ApplicationController
     @board = Board.new
   end
 
+  def create
+    Board.create(board_params)
+    redirect_to new_board_path
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:author_name, :title, :body)
+  end
+
+
 end
